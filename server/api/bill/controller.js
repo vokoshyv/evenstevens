f78f36b10c82e80a0c25aba01582394b2177a595
vokoshyv <ronsuij@gmail.com>
2015-05-23 17:41:25 -0700

    (feat) Start initial work on Redis server

--- server/api/bill/controller.js
+++ server/api/bill/controller.js
@@ -1,11 +1,13 @@
 /* 
 * @Author: hal
 * @Date:   2015-05-22 15:10:00
-* @Last Modified by:   Johnny Nguyen
-* @Last Modified time: 2015-05-22 17:26:46
+* @Last Modified by:   vokoshyv
+* @Last Modified time: 2015-05-23 16:59:00
 */
 
 'use strict';
+
+var client = require('../../db.js')
 
 exports.show = function(req, res) {
   var billname = req.params.billname;
@@ -14,6 +16,25 @@ exports.show = function(req, res) {
 
 exports.create = function(req, res) {
   //redis code here;
+  //
+  console.log("REACHED HEREEEREEEEEE");
+  
+  client.on('error', function(error){
+    console.log('Error while creating the Socket Connection. Error is: ', error);
+  });
+
+  client.set('vege', 'radish', redis.print);
+  client.set('fruit', 'APPLES', redis.print);
+
+  // client.get('vege', function(error, value){
+
+  //   if (error){
+  //     throw error;
+  //   }
+
+  //   console.log('The vegetable is = ' + value);
+
+  // })
 
 };
 
