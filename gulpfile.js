17f792a944b18d4eb21d0a7539a3623590051809
vokoshyv <ronsuij@gmail.com>
2015-05-26 20:27:52 -0700

    (feat) Sets up paths structure in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,25 @@
 * @Author: vokoshyv
 * @Date:   2015-05-26 17:12:39
 * @Last Modified by:   vokoshyv
-* @Last Modified time: 2015-05-26 19:15:26
+* @Last Modified time: 2015-05-26 20:27:40
 */
 
 'use strict';
 
-//require gulp plugins
+// require gulp plugins
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     livereload = require('gulp-livereload'),
     open = require('gulp-open'),
     shell = require('gulp-shell'),
     stylish = require('jshint-stylish');
+
+// set up paths 
+var paths = {
+  scripts: 'server/public/javascripts/*.js', 
+  server: 'server/*.js', 
+  styles: 'server/public/stylesheets/*.css',
+  billRoute: 'server/api/bill/*.js', 
+  html: 'server/public/index.html'
+}
 
