e318d301bc4563e5fe1080cd701538599cf1738d
vokoshyv <ronsuij@gmail.com>
2015-05-26 19:15:58 -0700

    (feat) Requires gulp plugins inside gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,16 @@
 * @Author: vokoshyv
 * @Date:   2015-05-26 17:12:39
 * @Last Modified by:   vokoshyv
-* @Last Modified time: 2015-05-26 17:15:50
+* @Last Modified time: 2015-05-26 19:15:26
 */
 
 'use strict';
+
+//require gulp plugins
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    livereload = require('gulp-livereload'),
+    open = require('gulp-open'),
+    shell = require('gulp-shell'),
+    stylish = require('jshint-stylish');
 
