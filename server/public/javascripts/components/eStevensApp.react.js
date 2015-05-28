b165badd339570471c0e3db4ba898321d801962d
Nathan Bailey <nbailey0001@gmail.com>
2015-05-27 20:26:28 -0700

    (feat) Adds bootstrap for styling

--- server/public/javascripts/components/eStevensApp.react.js
+++ server/public/javascripts/components/eStevensApp.react.js
@@ -2,7 +2,7 @@
 * @Author: Nathan Bailey
 * @Date:   2015-05-27 10:46:34
 * @Last Modified by:   Nathan Bailey
-* @Last Modified time: 2015-05-27 20:37:33
+* @Last Modified time: 2015-05-27 20:38:48
 */
 
 /** 
@@ -14,6 +14,9 @@ var NameInputForm = require('./NameInputForm.react');
 var CameraImageCapture = require('./CameraImageCapture.react');
 var React = require('react');
 var AppStore = require('../stores/AppStore');
+var Navbar= require('react-bootstrap').Navbar;
+var Nav = require('react-bootstrap').Nav;
+var NavItem = require('react-bootstrap').NavItem;
 
 var getAppState = function() {
   return {
@@ -38,12 +41,26 @@ var EvenStevensApp = React.createClass({
   componentWillUnmount: function() {
     AppStore.removeChangeListener(this._onChange);
   },
+
+
+
+
+
+
   render: function() {
     return (
       <div>
-        <h1>Evens Stevens</h1>
+        <Navbar brand='Even Stevens'  toggleNavKey={0}>
+    <Nav right eventKey={0}> {/* This is the eventKey referenced */}
+      <NavItem eventKey={1} href='#'>Link</NavItem>
+      <NavItem eventKey={2} href='#'>Link</NavItem>
+      
+    </Nav>
+  </Navbar>
+      <div className = "container">
         <NameInputForm userName={this.state.userName} />
         <CameraImageCapture imageBeingProcessed={this.state.imageBeingProcessed} userName={this.state.userName} />
+        </div>
       </div>
     );
   },
