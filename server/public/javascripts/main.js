e243c3507df70d6b0b2de0c0e4fb6911d797ad44
Michael Harris <mwharris333@gmail.com>
2015-05-28 15:27:11 -0700

    (feat) Add functions for sending test messages

--- server/public/javascripts/main.js
+++ server/public/javascripts/main.js
@@ -2,8 +2,115 @@
 * @Author: hal
 * @Date:   2015-05-22 14:00:21
 * @Last Modified by:   Michael Harris
-* @Last Modified time: 2015-05-28 19:22:16
+* @Last Modified time: 2015-05-28 19:25:34
 */
+'use strict';
+
+var name;
+
+/**
+ * resizing function
+ */
+// function resize (file, maxWidth, maxHeight, fn) {
+//     var reader = new FileReader();
+//     reader.readAsDataURL(file);
+//     reader.onload = function (event) {
+//         var dataUrl = event.target.result;
+//         var image = new Image();
+//         image.src = dataUrl;
+//         image.onload = function () {
+//             var resizedDataUrl = resizeImage(image, maxWidth, maxHeight, 0.7);
+//             fn(resizedDataUrl);
+//         };
+//     };
+// };
+ 
+/**
+ * resizing function
+ */ 
+// function resizeImage(image, maxWidth, maxHeight, quality) {
+//     var canvas = document.createElement('canvas');
+ 
+//     var width = image.width;
+//     var height = image.height;
+ 
+//     if (width > height) {
+//       if (width > maxWidth) {
+//         height = Math.round(height * maxWidth / width);
+//         width = maxWidth;
+//       }
+//     } else {
+//       if (height > max_height) {
+//         width = Math.round(width * maxHeight / height);
+//         height = maxHeight;
+//       }
+//     }
+ 
+//     canvas.width = width;
+//     canvas.height = height;
+ 
+//     var ctx = canvas.getContext("2d");
+//     ctx.drawImage(image, 0, 0, width, height);
+//     return canvas.toDataURL("image/jpeg", quality);
+// };
+
+console.log('main.js initiate socketClient');
+var socketClient = SocketClient('http://7c534c2c.ngrok.com');
+
+setTimeout(function(){
+  var data = {clientData:'someNewData'};
+  socketClient.send(data);
+}, 2000);
+
+setTimeout(function(){
+  var data = {clientData:'someOtherData'};
+  socketClient.send(data);
+}, 4000);
+
+// 
+// socketClient.send({partyname: 'tomsParty', 
+//   "diners": [
+//     {
+//       "diner": "tom",
+//       "itemIndex": [0, 1, 2]
+//     },
+//     {
+//       "diner": "ron",
+//       "itemIndex": [3,4]
+//     },
+//     {
+//       "diner": "bob",
+//       "itemIndex": [5, 6]
+//     }
+//   ]
+// });
+
+var InputForm = React.createClass({
+  getInitialState: function(){
+    return {message: ""};
+  },
+  handleInput: function(e){
+    e.preventDefault();
+    var name = React.findDOMNode(this.refs.name).value.trim();
+    if(!name) {
+      this.setState({message:"Please enter your name"});
+      return;
+    }
+    this.setState({message:""});
+    this.props.onInputSubmit({name:name});
+    React.findDOMNode(this.refs.name).value = '';
+    return;
+  },
+  render: function() {
+    return (
+      <form className ="inputForm" onSubmit={this.handleInput}>
+        <input type="text" placeholder="Enter your name" ref="name" />
+        <input type="submit" value="Keep it even" />
+        <div className ="errorBox"> {this.state.message}</div>
+      </form>
+    );
+  }
+});
 
 // uncomment when browserify is configured
 // var React = require('react');
@@ -11,7 +118,6 @@
 // var eStevensApp = require('./components/eStevensApp.react')
 // 
 // 
-'use strict';
 
 var React = require('react');
 var Router = require('react-router');
