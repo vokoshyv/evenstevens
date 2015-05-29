dedb3dc5e1d8743f0b5bd6d2f5fef828371936fa
Michael Harris <mwharris333@gmail.com>
2015-05-29 01:34:33 -0700

    (feat) Add rooms to socket (working)

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@
 * @Author: hal
 * @Date:   2015-05-22 10:53:35
 * @Last Modified by:   Michael Harris
-* @Last Modified time: 2015-05-28 19:23:50
+* @Last Modified time: 2015-05-29 01:25:55
 */
 
 // set up server variables
@@ -14,7 +14,15 @@ var bodyParser = require('body-parser');
 var bill = require('./api/bill/index');
 var app = express();
 
-app.set('port', (process.env.PORT || 3000));
+var http = require('http');
+var server = http.createServer(app);
+var io = require('socket.io')(server);
+
+var port = process.env.PORT || 3000;
+app.set('port', port);
+server.listen(port, function () {
+  console.log('Server listening on port', port);
+});
 
 // uncomment after placing your favicon in /public
 app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -31,6 +39,8 @@ app.use('*', function (req, res) {
 
 // app.use('/api/bills/', function(req, res){ socketServer.newRoom(req, res, bill); });
 // app.use('/api/bills', bill);
+
+// app.use('/api/bills', function(req, res, cb) {bill(req, res, function(){ return io;}) });
 
 // app.use('/', express.static(path.join(__dirname, 'public')));
 
@@ -65,8 +75,42 @@ app.use(function(err, req, res, next) {
   });
 });
 
+// Start server
+// app.listen(app.get('port'), function () {
+//   console.log('Server started: http://localhost:' + app.get('port') + '/');
+// });
+
 // start socket server
-console.log('app.js initiate socketServer STEP 1');
-var socketServer = require('./socketServer')(app);
+// console.log('app.js initiate socketServer STEP 1');
+// var socketServer = require('./socketServer')(app);
+
+var sockets = {};
+
+var namespace = '/';
+io.of(namespace).on('connection', function(socket){
+  socket.on('primaryUserFirstRun',  function(data) { onPrimaryUserFirstRun(socket, data) });
+  socket.on('standardUserFirstRun', function(data) { onStandardUserFirstRun(socket, data) });
+  socket.on('standardUserSecondRun', function(data) { onStandardUserSecondRun(socket, data) });
+  socket.on('disconnect', onDisconnect);
+});
+
+var socketLog = function(socket, data) {
+  console.log('SOCKET / connection', socket.id, socket.rooms, data);
+};
+
+var onPrimaryUserFirstRun = function (socket) {
+};
+
+var onStandardUserFirstRun = function (socket, data) {
+  socket.join(data.billname);
+};
+
+var onStandardUserSecondRun = function (socket, data) {
+  socketLog(socket, data);
+  io.to(data.billname).emit('dataFromServer', {dataFromServer: 'broadcast'});
+};
+
+var onDisconnect = function (socket) {
+};
 
 module.exports = app;
