aed14ce837a4f0b26222468ab5bc5c303c1e2394
Johnny Nguyen <hellojohnnguyen@gmail.com>
2015-05-29 14:34:40 -0700

    (feat) Delete test file

---
+++ server/public/javascripts/components/test.react.js
@@ -0,0 +1,25 @@
+/* 
+* @Author: Johnny Nguyen
+* @Date:   2015-05-29 12:05:34
+* @Last Modified by:   Johnny Nguyen
+* @Last Modified time: 2015-05-29 12:06:36
+*/
+
+var React = require('react');
+var AppActions = require('../actions/AppActions');
+
+var Test = React.createClass({
+
+  render: function() {
+    return (
+      <div>
+        test
+      </div>
+    );
+  }
+});
+
+module.exports = Test;
+
+
+
