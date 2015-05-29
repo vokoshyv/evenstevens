67163d365aec55dfc28dc58332e3ddd099acc250
Johnny Nguyen <hellojohnnguyen@gmail.com>
2015-05-28 17:33:44 -0700

    (feat) Add routing

--- server/public/javascripts/main.js
+++ server/public/javascripts/main.js
@@ -1,8 +1,8 @@
 /* 
 * @Author: hal
 * @Date:   2015-05-22 14:00:21
-* @Last Modified by:   Nathan Bailey
-* @Last Modified time: 2015-05-27 13:49:47
+* @Last Modified by:   Johnny Nguyen
+* @Last Modified time: 2015-05-28 17:33:12
 */
 
 // uncomment when browserify is configured
@@ -15,13 +15,26 @@
 
 
 var React = require('react');
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
 
 var EvenStevensApp = require('./components/eStevensApp.react.js')
 
-React.render(
-  <EvenStevensApp />,
-  document.getElementById('content')
+var Bill = require('./components/bill.react.js');
+var Test = require('./components/test.react.js');
+
+var routes = (
+  <Route handler={EvenStevensApp} path="/">
+    <Route path="/:bill" handler={Bill}/>
+  </Route>
 );
+
+Router.run(routes, Router.HistoryLocation, function (Handler) {
+  React.render(<Handler />, document.getElementById('content'));
+});
 
 //var socketClient = SocketClient('http://127.0.0.1:3000');
 
