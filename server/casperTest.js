678027ace14a6fbf3aa5002dbac05018ef20c41b
Michael Harris <mwharris333@gmail.com>
2015-06-08 12:09:22 -0700

    (feat) Add test for bundle.js loaded

--- server/casperTest.js
+++ server/casperTest.js
@@ -1,10 +1,34 @@
-var port = process.env.PORT || 3000;
-var host = 'http://localhost:' + port;
-casper.test.begin('Testing Even Stevens', 1, function(test){
+var host = 'http://localhost:3000';
+casper.test.begin('Testing EvenStevens.co', 2, function(test){
   casper.start(host);
 
   casper.then(function(){
-      test.assertTitle('Even Stevens', 'Even Stevens has correct title');
+    test.assertTitle('Even Stevens', 'Even Stevens has correct title');
+    this.waitForResource("bundle.js", function() {
+      test.pass("Found bundle.js");
+    }, function() {
+      test.fail("Did not find bundle.js");
+    });
+  });
+
+  casper.then(function(){
+    test.assertExists('#content', 'Found #content div element');
+    test.assertExists('#content', 'Found #content.container div element');
+    // test.assertExists('#content span', 'Found #helloWorld');
+
+    if (this.exists('div:first-of-type')) {
+      test.pass("Button found");
+    } else {
+      test.fail("Button not found");
+    }
+
+    // test.assertExists('form.form', 'Found form');
+    // test.assertExists('button .btn-lrg.btn-primary', 'Found button div element');
+    // test.assertExists('#helloWorld', '#helloWorld span element found');
+    // casper.fill('selector', {
+    //     'nameOfFormElement' : 'yourInput'
+    // }, submit);
+
   });
 
   casper.run(function(){
