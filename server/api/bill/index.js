cad677f48b8334bfa5b13e828b18e80031d269f8
Johnny Nguyen <hellojohnnguyen@gmail.com>
2015-06-13 15:12:45 -0700

    (cleanup) Remove unused route

--- server/api/bill/index.js
+++ server/api/bill/index.js
@@ -2,13 +2,13 @@
 * @Author: hal
 * @Date:   2015-05-22 11:52:34
 * @Last Modified by:   hal
-* @Last Modified time: 2015-06-12 19:29:10
+* @Last Modified time: 2015-06-13 15:11:43
 */
 
 var express = require('express');
 var controller = require('./controller');
 var router = express.Router();
 
-router.get('/:billName', controller.show);
+router.post('/:billName', controller.create);
 
 module.exports = router;
