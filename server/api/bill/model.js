6afaf8de36e32f17c38094fd06e85a4316736037
vokoshyv <ronsuij@gmail.com>
2015-06-16 12:11:29 -0700

    (d(fix) Changes comment in model.js to follow proper docblocker format

--- server/api/bill/model.js
+++ server/api/bill/model.js
@@ -2,13 +2,16 @@
 * @Author: Johnny Nguyen
 * @Date:   2015-05-27 19:34:23
 * @Last Modified by:   vokoshyv
-* @Last Modified time: 2015-06-15 11:18:01
+* @Last Modified time: 2015-06-16 12:10:54
 */
 
 'use strict';
 /**
- * [billModel description]
- * @return {[type]} [description]
+ * This is the model of how all of the receipts will be 
+ * stored inside of the Redis datastore; it is also the 
+ * format that will be sent back to the secondary users when
+ * they initially connect to the room. 
+ * @return {object} The party object itself
  */
 exports.billModel = function() {
   return {
